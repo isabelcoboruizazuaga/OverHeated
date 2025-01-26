4f05ef4775df1f5b9e9f9429aee749a46e80bfc6
Isa <isabelcoboruizazuaga@gmail.com>
2025-01-26 15:20:00 +0100

    added winning menu

---
+++ Assets/Scripts/Winnin.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winnin : MonoBehaviour
+{
+    [SerializeField] GameObject panelEnd;
+    [SerializeField] GameObject img1, img2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            panelEnd.SetActive(true);
+
+            if (GameObject.Find("Player1").GetComponent<Collider2D>().enabled)
+            {
+                img1.SetActive(true);
+                img2.SetActive(false);
+            }
+           if ( GameObject.Find("Player2").GetComponent<Collider2D>().enabled)
+            {
+                img2.SetActive(true);
+                img1.SetActive(false);
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+        if (!GameObject.Find("Player1").GetComponent<Collider2D>().enabled && !GameObject.Find("Player2").GetComponent<Collider2D>().enabled)
+        {
+            panelEnd.SetActive(true);
+            img2.SetActive(false);
+            img1.SetActive(false);
+        }
+    }
+}
